fcbdc49b39e30d66df1b222ccaeff24321b564e4
ramosha <haven.ramos@ofx.com>
2019-05-14 12:34:33 +1000

    tools

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  target: 'serverless'
-}
+const withTypescript = require("@zeit/next-typescript");
+const withSass = require("@zeit/next-sass");
+
+module.exports = withTypescript(
+  withSass({
+    webpack(config, options) {
+      return config;
+    },
+    cssModules: true,
+    sassLoaderOptions: {},
+  })
+);
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-const Index = () =>
-  <div>
-    <p>funny money</p>
-    <p>heard too much</p>
-		<h1>Empowerment ftw</h1>
-  </div>;
-
-export default Index;
