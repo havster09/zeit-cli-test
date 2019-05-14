d3481e1e5c5dca490a2afa4606d1479c157a8e48
ramosha <haven.ramos@ofx.com>
2019-05-14 11:41:39 +1000

    haven dev

---
+++ components/Layout.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import { withAmp } from 'next/amp';
+import * as css from "../styles.css";
+import "../styles.scss";
+
+type Props = {
+  title?: string,
+}
+
+const Layout: React.FunctionComponent<Props> = ({ children, title = 'This is the default title' }) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <style jsx global>{`
+        body {
+          font-family: Roboto, sans-serif;
+          padding: 30px;
+          color: #444;
+        }
+      `}</style>
+    </Head>
+    <header>
+      <nav className={css.example}>
+        <Link href='/'><a>Home</a></Link> | {' '}
+        <Link href='/list-class'><a>List Example</a></Link> | {' '}
+        <Link href='/list-fc'><a>List Example (as Functional Component)</a></Link> | {' '}
+        <Link href='/about'><a>About</a></Link> | {' '}
+      </nav>
+    </header>
+    {children}
+    <footer>
+      <hr />
+      <span style={{color: 'pink'}}>I'm here to stay (Footer)</span>
+    </footer>
+  </div>
+)
+
+export default withAmp(Layout, { hybrid: true });
--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  target: 'serverless'
-}
+const withTypescript = require('@zeit/next-typescript')
+const withCSS = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
+
+module.exports = withTypescript(withSass(withCSS({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  }})));
